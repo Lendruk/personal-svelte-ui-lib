45ba5d5e503a58ba7095e36a37d608193e0b785f
Lendruk <fenixeye3@gmail.com>
2025-06-01 21:27:51 +0100

    Expose more events for text area

--- src/lib/components/TextArea.svelte
+++ src/lib/components/TextArea.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
 	export let value: string;
+	export let onChange: (event: Event) => void = () => {};
+	export let onKeyDown: (event: KeyboardEvent) => void = () => {};
+	export let onKeyUp: (event: KeyboardEvent) => void = () => {};
 	export let placeholder: string | undefined = undefined;
 </script>
 
 <textarea
-	class="focus:outline-none bg-surface-color border-red-950 pl-2 border-2 rounded-md shadow-xl"
+	class="bg-surface-color rounded-md border-2 border-red-950 pl-2 shadow-xl focus:outline-none"
 	bind:value
+	on:change={onChange}
+	on:keydown={onKeyDown}
+	on:keyup={onKeyUp}
 	{placeholder}
 ></textarea>
