7bc6499a3bef0681c7ed138f54d08535e6df970d
lendruk <fenixeye3@gmail.com>
2026-04-12 15:32:18 +0100

    Fix z-index

--- src/lib/components/VerticalDrawer.svelte
+++ src/lib/components/VerticalDrawer.svelte
@@ -20,12 +20,12 @@
 </script>
 
 {#if isDrawerOpen}
-	<div class="fixed inset-0 z-40 flex">
+	<div class="fixed inset-0 flex" style="z-index: 10000;">
 		<!-- svelte-ignore a11y_click_events_have_key_events a11y_no_static_element_interactions -->
 		<div onclick={close} class="flex-1 animate-fade-in bg-black/50 backdrop-blur-sm"></div>
 		<div
-			class="border-main/20 flex h-full w-full max-w-md animate-slide-in-right
-			flex-col border-l
+			class="flex h-full w-full max-w-md animate-slide-in-right flex-col
+			border-l border-main/20
 			bg-dark-contrast shadow-2xl
 			shadow-black/50"
 		>
